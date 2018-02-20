60630c73061f39294bb7337845ffcd887f4291c3
Tejaswini Patil <tejaswini26patil@gmail.com>
2018-02-20 16:26:29 +0530

    test commit

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -2,7 +2,7 @@ class LinksController < ApplicationController
   before_action :set_link, only: [:show, :edit, :update, :destroy]
    helper_method :sort_column, :sort_direction
 
-  #
+  
   # GET /links
   # GET /links.json
    def index
